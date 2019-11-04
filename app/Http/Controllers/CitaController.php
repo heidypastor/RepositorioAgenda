6f898504b01f5b89aaee7d9a3f4c7a328efb03ff
Heidy <heidypastor13@gmail.com>
2019-11-04 13:48:49 -0500

    Agregue cambios de la rama Heidy y borre las imagenes

--- app/Http/Controllers/CitaController.php
+++ app/Http/Controllers/CitaController.php
@@ -1,10 +1,8 @@
 <?php
-
 namespace Ensayo\Http\Controllers;
-
 use Illuminate\Http\Request;
 use Ensayo\Cita;
-
+use Ensayo\Http\Requests\StoreCitaRequest;
 class CitaController extends Controller
 {
     /**
@@ -15,10 +13,8 @@ class CitaController extends Controller
     public function index()
     {
         $citas = Cita::all();
-
         return view("Citas.index", compact("citas"));
     }
-
     /**
      * Show the form for creating a new resource.
      *
@@ -28,29 +24,26 @@ class CitaController extends Controller
     {
         return view("Citas.create");
     }
-
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreCitaRequest $request)
     {
         if($request->hasFile("Usuario")){
             $file = $request->file("Usuario");
             $name = time().$file->getClientOriginalName();
             $file->move(public_path()."/images/",$name);
         }
-
         $fecha = new Cita();
         $fecha->fecha = $request->input("fecha");
         $fecha->Usuario = $name;
         $fecha->slug = $name.now();
         $fecha->save();
-        return "Los datos han sido guardados";
+        return redirect()->route("Cita.index");
     }
-
     /**
      * Display the specified resource.
      *
@@ -63,7 +56,6 @@ class CitaController extends Controller
     
         return view("Citas.show", compact("cita"));
     }
-
     /**
      * Show the form for editing the specified resource.
      *
@@ -73,11 +65,8 @@ class CitaController extends Controller
     public function edit($slug)
     {
         $cita = Cita::where("slug","=",$slug)->firstOrFail();
-
         return view("Citas.edit", compact("cita"));
-
     }
-
     /**
      * Update the specified resource in storage.
      *
@@ -97,17 +86,20 @@ class CitaController extends Controller
             $file->move(public_path()."/images/",$name);
         }
         $cita->save();
-        return "Cita actualizada correctamente";
+        return redirect()->route("Cita.show", [$cita->slug]);
     }
-
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($slug)
     {
-        //
+        $cita = Cita::where("slug","=",$slug)->firstOrFail();
+        $file_path = public_path()."/images/".$cita->usuario;
+        unlink($file_path);
+        $cita->delete();
+        return redirect()->route("Cita.index");;
     }
 }
